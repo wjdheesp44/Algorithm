9a207726d8c0c54ff0bd3ced9c8053248e0922e1
Heesu Jeong <huissppea@gmail.com>
2023-03-04 22:37:45 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,10 @@
+num = list(map(int, input().split()))
+ascending = [1, 2, 3, 4, 5, 6, 7, 8]
+descending = [8, 7, 6, 5, 4, 3, 2, 1]
+
+if (num == ascending):
+  print("ascending")
+elif (num == descending):
+  print("descending")
+else:
+  print("mixed")
