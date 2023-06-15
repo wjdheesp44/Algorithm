b61418cfdc34cbece6be820658d8fd90263c74f1
Heesu Jeong <huissppea@gmail.com>
2023-06-15 23:48:25 +0900

    [unrated] Title: n의 배수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181937. n의 배수/n의 배수.py
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.py
@@ -1,8 +1,5 @@
 def solution(num, n):
-    while True:
-        if num % n > n:
-            continue
-        elif num % n < n and num % n > 0:
-            return 0
-        else:
-            return 1
+    if num % n == 0:
+        return 1
+    else:
+        return 0
