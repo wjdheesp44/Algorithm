421d1fc9ef7f7979eb1c9de517bf64030f1a3fec
Heesu Jeong <huissppea@gmail.com>
2023-07-02 23:27:13 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,8 @@
+def solution(a, b, c):
+    answer = 0
+    if a != b and a != c and b != c:
+        return a + b + c
+    elif a == b and a == c and b == c:
+        return (a + b + c)*(a**2 + b**2 + c**2)*(a**3 + b**3 + c**3)
+    else:
+        return (a + b + c)*(a**2 + b**2 + c**2)
