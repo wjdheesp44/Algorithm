0f282b79662e51ab25855d9c56cbecb20e1f0d87
Heesu Jeong <huissppea@gmail.com>
2023-07-16 21:37:10 +0900

    [Silver V] Title: 뒤집기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,25 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-number = input()
+s = input()
+cnt = 0
 
-arr = list(map(int, number[:len(number)-1]))
-
-cnt0 = 0
-cnt1 = 0
-
-for i in range(len(arr)-1):
-    if arr[i] == 1 and arr[i+1] == 0:
-        cnt1 += 1
-    elif arr[i] == 0 and arr[i+1] == 1:
-        cnt0 += 1
-
-if arr[len(arr)-1] == 1:
-    cnt1 += 1
-elif arr[len(arr)-1] == 0:
-    cnt0 += 1
-
-if cnt1 > cnt0:
-    print(cnt0)
-else:
-    print(cnt1)
+for i in range(len(s)-1):
+    if s[i] != s[i+1]:
+        cnt += 1
+print(cnt//2)
