3aa786b362a695f3438e26a20c7460e00151e151
Heesu Jeong <huissppea@gmail.com>
2024-02-23 16:16:59 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME
+from ANIMAL_INS i
+join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
+where (i.SEX_UPON_INTAKE like "Intact%") and (o.SEX_UPON_OUTCOME like "Spayed%" or 
+                                              o.SEX_UPON_OUTCOME like "Neutered%")
+order by i.ANIMAL_ID
