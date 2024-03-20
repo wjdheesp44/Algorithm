73a339566fb6e52b9c9a3bfe68361fef2c61dee2
Heesu Jeong <huissppea@gmail.com>
2024-03-21 00:00:47 +0900

    [level 2] Title: 구명보트, Time: 7.88 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,23 +1,15 @@
 def solution(people, limit):
-    lst = []
     answer = 0
-    i = 0
     people.sort()    
     
     start = 0
     end = len(people) - 1
     
-    while True:
-        if start > end:
-            break
-        elif start == end:
-            answer += 1
-            break
-            
+    while start <= end:
         if people[start] + people[end] <= limit:
             start += 1
-        
-        answer += 1
-        end -= 1
             
+        end -= 1
+        answer += 1
+        
     return answer
