01563cfa12a082ec84d9aa96e80bf68160217f7e
Heesu Jeong <huissppea@gmail.com>
2024-03-25 00:11:20 +0900

    [Silver II] Title: 키로거, Time: 936 ms, Memory: 41984 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.py
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -1,20 +1,21 @@
 n = int(input())
 
-for _ in range(n):
+for i in range(n):
     l_list = []
     r_list = []
     data = input()
     for i in data:
-        if i == '-':
-            if l_list: #왼쪽 스택에 있을 경우
+        if i == '-': 
+            if l_list:
                 l_list.pop()
-        elif i == '<': # 왼스택에 있는 문자 오른쪽 스택으로
+        elif i == '<':
             if l_list:
                 r_list.append(l_list.pop())
-        elif i == '>': # 오스택에 있는 문자 왼쪽 스택으로
+        elif i == '>':
             if r_list:
                 l_list.append(r_list.pop())
         else:
             l_list.append(i)
-    l_list.extend(reversed(r_list)) # 오른쪽 스택에 있는 문자들은 뒤집어서 붙여야 한다.
+    
+    l_list.extend(reversed(r_list))
     print(''.join(l_list))
