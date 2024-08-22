2d50329948d0049363901c8ac4f448cc82072e05
Heesu Jeong <huissppea@gmail.com>
2024-08-22 23:49:26 +0900

    [Silver II] Title: 나무 자르기, Time: 164 ms, Memory: 47864 KB -BaekjoonHub

---
+++ 백준/Silver/14247. 나무 자르기/나무 자르기.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+arr = []
+ans = 0
+
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+for i in range(n):
+    arr.append([a[i],b[i]])
+
+arr = sorted(arr, key = lambda x: [x[1]])
+
+for i in range(n):
+    ans += arr[i][0] + (i * arr[i][1])
+
+print(ans)
