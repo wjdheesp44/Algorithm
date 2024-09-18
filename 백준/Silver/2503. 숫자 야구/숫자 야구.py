e0f9eef01aea5c64803ca1ae7d4fa7c14d02ef06
Heesu Jeong <huissppea@gmail.com>
2024-09-19 05:17:55 +0900

    [Silver III] Title: 숫자 야구, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,24 +1,36 @@
-from itertools import permutations
+import sys
+input = sys.stdin.readline
+
 N = int(input())
+result = []
 
-data = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-num = list(permutations(data, 3))
+for i in range(1, 10):
+    for j in range(1, 10):
+        if i == j:
+            continue
+        for k in range(1, 10):
+            if i == k or j == k:
+                continue
+            else:
+                result.append((i, j, k))
 
-for _ in range(N):
-    n, s, b = map(int, input().split())
-    n = list(str(n))
-    rmcnt = 0
-    for i in range(len(num)):
-        strike = ball = 0
-        i -= rmcnt # num[0] 부터 시작
+for cnt in range(N):
+    num, strike, ball = map(int, input().split())
+    num_first = num // 100
+    num_sec = (num // 10) % 10
+    num_third = num % 10
+    input_num = (num_first, num_sec, num_third)
+    new_result = []
+    for number in result:
+        tmp_strike = 0
+        tmp_ball = 0
         for j in range(3):
-            if num[i][j] == n[j]:
-                strike += 1
-            elif n[j] in num[i]:
-                ball += 1
-            
-        if (strike != s) or (ball != b):
-            num.remove(num[i])
-            rmcnt += 1
+            if number[j] == input_num[j]:
+                tmp_strike += 1
+            elif number[j] in input_num:
+                tmp_ball += 1
+        if tmp_strike == strike and tmp_ball == ball:
+            new_result.append(number)
+    result = new_result
 
-print(len(num))
+print(len(result))
