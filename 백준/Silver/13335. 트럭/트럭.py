2c287ae9f6157af58513197293cc959919dbae9b
Heesu Jeong <huissppea@gmail.com>
2024-09-21 03:09:28 +0900

    [Silver I] Title: 트럭, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -1,17 +1,22 @@
-import sys
-input = sys.stdin.readline
+n, w, L = map(int, input().split())  # 트럭 수, 다리 길이, 최대하중 입력
+truck_weights = list(map(int, input().split()))  # 트럭 무게 입력
 
-n, w, l = map(int, input().split())
-q = list(map(int, input().split()))
- 
-b = [0] * w
-time = 0
-while b: 
-	time += 1
-	b.pop(0) 
-	if q: 
-		if sum(b) + q[0] <= l:
-			b.append(q.pop(0))
-		else:
-			b.append(0)
+bridge = [0] * w  # 다리 위에 올라가 있는 트럭의 상태를 저장하는 큐
+total_weight = 0  # 현재 다리 위 트럭들의 무게 합
+time = 0  # 경과 시간
+
+for truck in truck_weights:
+    while True:
+        time += 1
+        total_weight -= bridge.pop(0)
+
+        # 다리 위에 트럭을 올릴 수 있으면 올리고 반복 종료
+        if total_weight + truck <= L:
+            bridge.append(truck)
+            total_weight += truck
+            break
+        else:
+            bridge.append(0)
+
+time += w
 print(time)
