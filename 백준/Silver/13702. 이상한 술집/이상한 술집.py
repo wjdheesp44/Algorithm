febfa0d08ce869bf7a6b51fa292abf1918ed7a26
Heesu Jeong <huissppea@gmail.com>
2024-09-27 03:24:20 +0900

    [Silver II] Title: 이상한 술집, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13702. 이상한 술집/이상한 술집.py
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.py
@@ -1,20 +1,20 @@
-
 import sys
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
-c = [int(input()) for _ in range(n)]
-start, end = 1, max(c)
+N, K = map(int, input().split())
+alcohols = [int(input()) for _ in range(N)]
+
+start, end = 1, sum(alcohols) // K
+result = 0
 
 while start <= end:
-    temp = 0
     mid = (start + end) // 2
-    for i in c:
-        temp += (i // mid)
-    if temp >= k:
-        result = mid
-        start = mid+1
+    cnt = sum(alcohol // mid for alcohol in alcohols if mid > 0)
+
+    if cnt >= K:
+        result = mid  # 가능한 최대 용량을 저장
+        start = mid + 1  # 더 큰 용량을 찾기 위해 범위를 조정
     else:
-        end = mid-1
+        end = mid - 1  # 분배할 수 없는 경우 범위를 줄임
 
 print(result)
