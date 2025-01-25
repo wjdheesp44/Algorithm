771f2978cf6b50b84830af79428cfa4935dae69f
Heesu Jeong <huissppea@gmail.com>
2025-01-25 11:45:39 +0900

    [Gold II] Title: 주식, Time: 152 ms, Memory: 112272 KB -BaekjoonHub

--- 백준/Gold/12014. 주식/주식.py
+++ 백준/Gold/12014. 주식/주식.py
@@ -2,37 +2,30 @@ import sys
 
 input = sys.stdin.readline
 
-def binary_search(lis, target):
-    left, right = 0, len(lis)
-    while left < right:
-        mid = (left + right) // 2
-        if lis[mid] < target:
-            left = mid + 1
-        else:
-            right = mid
-    return left
-
 T = int(input())
 
-for t in range(1, T + 1):
+for t in range(1, T+1):
 
     N, K = map(int, input().split())
     chart = list(map(int, input().split()))
 
-    # LIS를 저장할 리스트
     lis = []
+    
+    for k in range(N):
+        start, end = 0, len(lis)
 
-    for num in chart:
-        # 직접 구현한 이분 탐색으로 위치를 찾음
-        pos = binary_search(lis, num)
-        if pos < len(lis):
-            # lis[pos]가 존재하면 대체
-            lis[pos] = num
+        while start < end:
+            mid = (start + end) // 2
+            if lis[mid] < chart[k]:
+                start = mid + 1
+            else:
+                end = mid
+
+        if start < len(lis):
+            lis[start] = chart[k]
         else:
-            # lis[pos]가 없으면 새 값을 추가
-            lis.append(num)
+            lis.append(chart[k])
 
-    # 결과 출력
     print(f'Case #{t}')
     if len(lis) >= K:
         print(1)
