ef17e402489a6f8941498c9644a7dc4ad9cddd4f
Heesu Jeong <huissppea@gmail.com>
2025-03-10 23:36:49 +0900

    [Gold V] Title: 내려가기, Time: 268 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -2,31 +2,24 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+graph = list(map(int, input().split()))
+dp_small = graph[:]
+dp_big = graph[:]
 
-# 첫 번째 줄 입력 받기
-arr = list(map(int, input().split()))
-maxDP = arr[:]  # 최대값 DP 배열 초기화
-minDP = arr[:]  # 최소값 DP 배열 초기화
+for i in range(N-1):
+    graph = list(map(int, input().split()))
 
-# 나머지 줄 처리
-for _ in range(N - 1):
-    arr = list(map(int, input().split()))
-    
-    # 현재 줄에서 계산한 값을 갱신할 때 이전 값을 참조해야 하므로 임시 변수 사용
-    newMaxDP = [0] * 3
-    newMinDP = [0] * 3
+    temp_small = [0] * 3
+    temp_small[0] = min(dp_small[0], dp_small[1]) + graph[0]
+    temp_small[1] = min(dp_small[0], dp_small[1], dp_small[2]) + graph[1]
+    temp_small[2] = min(dp_small[1], dp_small[2]) + graph[2]
 
-    newMaxDP[0] = arr[0] + max(maxDP[0], maxDP[1])
-    newMaxDP[1] = arr[1] + max(maxDP[0], maxDP[1], maxDP[2])
-    newMaxDP[2] = arr[2] + max(maxDP[1], maxDP[2])
+    temp_big = [0] * 3
+    temp_big[0] = max(dp_big[0], dp_big[1]) + graph[0]
+    temp_big[1] = max(dp_big[0], dp_big[1], dp_big[2]) + graph[1]
+    temp_big[2] = max(dp_big[1], dp_big[2]) + graph[2]
 
-    newMinDP[0] = arr[0] + min(minDP[0], minDP[1])
-    newMinDP[1] = arr[1] + min(minDP[0], minDP[1], minDP[2])
-    newMinDP[2] = arr[2] + min(minDP[1], minDP[2])
+    dp_small = temp_small[:]
+    dp_big = temp_big[:]
 
-    # 계산된 값을 DP 배열에 갱신
-    maxDP = newMaxDP[:]
-    minDP = newMinDP[:]
-
-# 마지막 줄에서 최대값과 최소값 출력
-print(max(maxDP), min(minDP))
+print(max(dp_big[0], dp_big[1], dp_big[2]), min(dp_small[0], dp_small[1], dp_small[2]))
