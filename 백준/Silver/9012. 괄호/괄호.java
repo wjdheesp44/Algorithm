72616835fa7cc86745f4ef36d081dcdc8110e8b2
Heesu Jeong <huissppea@gmail.com>
2025-07-30 01:11:04 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,32 +1,36 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
+    static int N;
+    static Deque<Character> stack;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Deque<String> stack;
+        N = Integer.parseInt(st.nextToken());
+        char[] arr;
 
-        int N = Integer.parseInt(st.nextToken());
-        String[] line;
-        A : for (int i = 0; i < N; i++) {
-            line = br.readLine().split("");
+        for (int i = 0; i < N; i++) {
+            String result = "YES";
+            arr = br.readLine().toCharArray();
             stack = new ArrayDeque<>();
 
-            for (String s : line) {
-                if (s.equals("(")) {
+            for (char s : arr) {
+                if (s == '(') {
                     stack.push(s);
-                } else if (s.equals(")") && !stack.isEmpty()) {
-                    stack.pop();
-                } else {
-                    sb.append("NO").append("\n");
-                    continue A;
+                } else if (s == ')') {
+                    if (!stack.isEmpty()) stack.pop();
+                    else result = "NO";
                 }
             }
-            if (stack.isEmpty()) sb.append("YES").append("\n");
-            else sb.append("NO").append("\n");
+            if (!stack.isEmpty()) result = "NO";
+            sb.append(result).append("\n");
         }
         System.out.println(sb);
     }
