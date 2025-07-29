6fc9ab56f9744abf8b41555165be2a5bc36fe5cc
Heesu Jeong <huissppea@gmail.com>
2025-07-30 01:54:28 +0900

    [Silver IV] Title: 큐, Time: 172 ms, Memory: 19812 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+    static Deque<Integer> q = new ArrayDeque<>();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            if (str.equals("push")) {
+                q.offer(Integer.parseInt(st.nextToken()));
+            } else if (str.equals("pop")) {
+                if (q.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(q.poll()).append("\n");
+                }
+            } else if (str.equals("size")) {
+                sb.append(q.size()).append("\n");
+            } else if (str.equals("empty")) {
+                if (q.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if (str.equals("front")) {
+                if (q.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(q.peek()).append("\n");
+                }
+            } else if (str.equals("back")) {
+                if (q.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(q.peekLast()).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
