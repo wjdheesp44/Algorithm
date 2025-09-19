382839726e96a62a36eb20ffeb9f5539e941e021
Heesu Jeong <huissppea@gmail.com>
2025-09-20 04:08:32 +0900

    [level 1] Title: K번째수, Time: 0.98 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,13 +1,10 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        // System.out.println(commands.length);
-        // int[] answer = {};
         int[] answer = new int[commands.length];
         int i = 0;
         for (int[] command : commands) {
             int arrSize = command[1] - command[0] + 1;
-            // System.out.println(arrSize);
             int[] arr = new int[arrSize];
             System.arraycopy(array, command[0]-1, arr, 0, arrSize);
             Arrays.sort(arr);
