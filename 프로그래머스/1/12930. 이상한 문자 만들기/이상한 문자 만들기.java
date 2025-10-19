584606d7c425f825b469905116c7be353224d4f4
Heesu Jeong <huissppea@gmail.com>
2025-10-19 16:55:16 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append(" ");
+                cnt = 0;
+                continue;
+            } else if (cnt % 2 == 0) {
+                sb.append(Character.toUpperCase(s.charAt(i)));
+            } else {
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+            cnt++;
+        }
+        return sb.toString();
+    }
+}
