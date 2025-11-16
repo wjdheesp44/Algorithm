78ddb1a417bfa6eae39663ff3cf631b6d558bad4
Heesu Jeong <huissppea@gmail.com>
2025-11-16 22:23:56 +0900

    [Silver V] Title: 돌 게임, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if (N % 2 == 0) {
+			System.out.println("CY");
+		} else System.out.println("SK");
+	}
+}
